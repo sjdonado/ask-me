fa06fb3b26a686ba74e5bae099a4db4e4f2acda1
manulasker <emanuelule2828@gmail.com>
2020-06-27 15:47:03 -0500

    add Notebook and some utility scripts

---
+++ notebooks/download_data.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "downloading data"
+wget 'http://files.srl.inf.ethz.ch/data/py150.tar.gz'
+gunzip -c py150.tar.gz | tar xopf -
+rm py150.tar.gz
+mkdir -p data/pyast
+mv python50k_eval.json data/pyast/python50k_eval.json
+mv python100k_train.json data/pyast/python100k_train.json
