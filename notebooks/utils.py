0702b57d3123eebef1ac26dce120bca97609c99e
manulasker <emanuelule2828@gmail.com>
2020-06-27 23:13:31 -0500

    feat: Language Modeling for python understanding

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -2,50 +2,6 @@ import spacy
 import re
 NLP = spacy.load("en_core_web_sm")
 
-from tqdm import tqdm
-from itertools import islice
-import time
-
-# hack for tqdm
-tqdm.monitor_interval = 0
-
-
-class Logger:
-    def __init__(self):
-        self.ct = time.clock()
-        self.should_log = False
-
-    def reset_time(self):
-        self.ct = time.clock()
-
-    def log_time_s(self, label):
-        self.__log_time__(label, 1)
-
-    def log_time_ms(self, label):
-        self.__log_time__(label, 1000)
-
-    def __log_time__(self, label, multiplier):
-        if self.should_log:
-            print("{}: {}".format(label, multiplier * (time.clock() - self.ct)))
-            self.ct = time.clock()
-
-
-logger = Logger()
-
-
-def tqdm_lim(iter, total=None, lim=None):
-    if (total is None) and (lim is None):
-        return tqdm(iter)
-
-    right = 1000000000
-    if total is not None:
-        right = min(right, total)
-
-    if lim is not None:
-        right = min(right, lim)
-
-    return tqdm(islice(iter, 0, right), total=right)
-
 def get_regex_expression():
     """
     Generate some regex expression 
@@ -87,6 +43,7 @@ def remove_unnecesary_text(text, regex):
     Returns:
         text -- python string
     """
+    text = text.replace("\\", "")
     return re.sub(regex, ' ', text, flags = re.M | re.I)
 
 # Remove all whitespace characters
